5feb8c397b8572d0f6888395ba77ddb95c3afc5b
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-16 10:59:15 -0400

    Store address fields (to, from, cc, bcc, reply-to) as comma separated strings in the database.
    Flip assert_equal arguments for Email tests.

--- lib/mailhopper/queue.rb
+++ lib/mailhopper/queue.rb
@@ -5,14 +5,20 @@ module Mailhopper
 
     def deliver!(mail)
       Base.email_class.create({
-        :to_address => mail.to.to_s,
-        :from_address => mail.from.to_s,
-        :cc_address => mail.cc.to_s,
-        :bcc_address => mail.bcc.to_s,
-        :reply_to_address => mail.reply_to.to_s,
-        :subject => mail.subject,
-        :content => mail.to_s
+        :to_address       => address_to_s(mail.to),
+        :from_address     => address_to_s(mail.from),
+        :cc_address       => address_to_s(mail.cc),
+        :bcc_address      => address_to_s(mail.bcc),
+        :reply_to_address => address_to_s(mail.reply_to),
+        :subject          => mail.subject,
+        :content          => mail.to_s
       })
+    end
+
+    private
+
+    def address_to_s(field)
+      field.join(',') if field
     end
   end
 end
