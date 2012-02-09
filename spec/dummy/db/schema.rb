1df342d28034c8253973b60afb6bede002224a50
Dan Gebhardt <dan@cerebris.com>
2012-02-09 18:28:42 -0500

    updated schema with Rails 3.2

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -21,8 +22,8 @@ ActiveRecord::Schema.define(:version => 20110916191655) do
     t.text     "bcc_address"
     t.text     "content"
     t.datetime "sent_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
 end
