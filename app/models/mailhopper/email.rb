8a8c9eaedd6b07703a3a20cb3f30f93b21d2fdd6
Dan Gebhardt <dan@cerebris.com>
2012-05-07 22:22:18 -0400

    added create_from_mail() to Mailhopper::Email so that email-specific content could be removed from Mailhopper::Queue

--- app/models/mailhopper/email.rb
+++ app/models/mailhopper/email.rb
@@ -15,5 +15,31 @@ module Mailhopper
       self.sent_at = Time.now
       self.save!
     end
+
+    class << self
+      def create_from_mail(mail)
+        create({
+          :to_address       => address_to_s(mail.to),
+          :from_address     => address_to_s(mail.from),
+          :cc_address       => address_to_s(mail.cc),
+          :bcc_address      => address_to_s(mail.bcc),
+          :reply_to_address => address_to_s(mail.reply_to),
+          :subject          => mail.subject,
+          :content          => mail.to_s
+        })
+      end
+
+    private
+
+      def address_to_s(field)
+        if field
+          if field.is_a?(Array)
+            field.join(',') unless field.empty?
+          else
+            field
+          end
+        end
+      end
+    end
   end
 end
--- lib/mailhopper/queue.rb
+++ lib/mailhopper/queue.rb
@@ -4,27 +4,7 @@ module Mailhopper
     end
 
     def deliver!(mail)
-      Base.email_class.create({
-        :to_address       => address_to_s(mail.to),
-        :from_address     => address_to_s(mail.from),
-        :cc_address       => address_to_s(mail.cc),
-        :bcc_address      => address_to_s(mail.bcc),
-        :reply_to_address => address_to_s(mail.reply_to),
-        :subject          => mail.subject,
-        :content          => mail.to_s
-      })
+      Base.email_class.create_from_mail(mail)
     end
-
-    private
-
-      def address_to_s(field)
-        if field
-          if field.is_a?(Array)
-            field.join(',') unless field.empty?
-          else
-            field
-          end
-        end
-      end
   end
 end
