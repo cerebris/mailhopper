55922ff8ea331afb706ce51918084b2658fd4a2b
Dan Gebhardt <dan@cerebris.com>
2013-12-22 07:15:34 -0500

    config.i18n.enforce_available_locales not supported in active_support 3.2.3

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -38,7 +38,4 @@ Dummy::Application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
-
-  # Locale validation approach
-  config.i18n.enforce_available_locales = true
 end
